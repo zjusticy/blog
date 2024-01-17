7838c2b242ca3842b4daa47e3fcc1f093582f866
zjusticy <stevens123321@outlook.com>
2024-01-17 12:36:58 -0800

    Update next config file

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
-//next.config.js
-module.exports = {
-  webpack: function (config) {
-    // config.module.rules.push({
-    //   test: /\.md$/,
-    //   use: 'raw-loader',
-    // });
-    // config.node = {
-    //   fs: "empty",
-    // };
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
-    return config;
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
+
+module.exports = nextConfig;
